c1df410734f46de91e3a94f12ca8e5cdbf0fef7a
kpflugshaupt <kpflugshaupt@gmail.com>
2019-04-15 18:00:26 +0200

    switch off file logging (comented out)

--- pa_lib/log.py
+++ pa_lib/log.py
@@ -16,28 +16,28 @@ from pa_lib.const import PA_LOG_DIR
 def _init():
     log_path = PA_LOG_DIR
     log_file = 'pa_log.txt'
-    
+
     logger = lg.getLogger('pa_log')
     logger.setLevel(lg.INFO)
-    
-    # File handler
-    file_hdl = WatchedFileHandler(log_path + log_file)
-    file_hdl.setLevel(lg.INFO)
-    
+
+    # # File handler
+    # file_hdl = WatchedFileHandler(log_path + log_file)
+    # file_hdl.setLevel(lg.INFO)
+
     # Stream handler (writes to STDERR)
     stream_hdl = lg.StreamHandler()
     stream_hdl.setLevel(lg.INFO)
-    
+
     # Define format, add to file handler
     log_fmt = lg.Formatter(fmt='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    file_hdl.setFormatter(log_fmt)
+    # file_hdl.setFormatter(log_fmt)
     stream_hdl.setFormatter(log_fmt)
-    
+
     # Add handlers (only once)
     if not logger.hasHandlers():
-        logger.addHandler(file_hdl)
+        # logger.addHandler(file_hdl)
         logger.addHandler(stream_hdl)
-        
+
     return logger
 
 pa_logger = _init()
@@ -50,17 +50,17 @@ def warn(msg):
 
 def err(msg):
     pa_logger.error(msg)
-    
+
 class time_log(ContextDecorator):
     def __init__(self, name):
         self.name       = name
         self.start_time = None
         self.start_cpu  = None
-        
+
     def __enter__(self):
         self.start_time = time.perf_counter()
         self.start_cpu  = time.process_time()
-        
+
     def __exit__(self, exc_type, exc, exc_tb):
         elapsed = time.perf_counter() - self.start_time
         cpu     = time.process_time() - self.start_cpu
@@ -75,11 +75,11 @@ if __name__ == "__main__":
     info('-- Testing pa_log module - info')
     warn('-- Testing pa_log module - warn')
     err ('-- Testing pa_log module - err')
-    
+
     @time_log('decorated function')
     def test_function():
         time.sleep(0.2)
     test_function()
-    
+
     with time_log('timed context'):
         time.sleep(0.3)
