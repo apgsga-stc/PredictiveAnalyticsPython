3cd46cdb7f9f042199f099966cd31260624ae646
kpflugshaupt <kpflugshaupt@gmail.com>
2019-12-12 18:43:11 +0100

    Refactor to fix change-on-copy warning

--- axinova/spr_prepare.py
+++ axinova/spr_prepare.py
@@ -6,6 +6,7 @@ file_dir = Path.cwd()
 parent_dir = file_dir.parent
 sys.path.append(str(parent_dir))
 
+import pandas as pd
 from collections import namedtuple
 from typing import Dict
 
@@ -58,86 +59,101 @@ CantonMapping = Dict[str, str]
 
 
 def map_stations(
-    canton: str = None, cantons: CantonMapping = None, nr_ranges: StationMapping = None
-) -> None:
+    data: pd.DataFrame,
+    canton: str = None,
+    cantons: CantonMapping = None,
+    nr_ranges: StationMapping = None,
+) -> pd.DataFrame:
     """
     Map spr_data records to Intervista Station names in-place, either per canton by
     Flaechennummer ranges NrRange(low, high), or by directly mapping from canton
     to station
     """
-    global spr_data
+    mapped_data = data.copy()
     if nr_ranges is not None and canton is not None:
         for nr_range, station_name in nr_ranges.items():
-            target_rows = (spr_data["Gebiet Code"] == canton) & (
-                spr_data["Nummer"].isin(range(nr_range.low, nr_range.high + 1))
+            station_rows = (data["Gebiet Code"] == canton) & (
+                data["Nummer"].isin(range(nr_range.low, nr_range.high + 1))
             )
-            spr_data.loc[target_rows, "Station"] = station_name
+            mapped_data.loc[station_rows, "Station"] = station_name
     elif cantons is not None:
         for canton, station_name in cantons.items():
-            target_rows = spr_data["Gebiet Code"] == canton
-            spr_data.loc[target_rows, "Station"] = station_name
+            station_rows = data["Gebiet Code"] == canton
+            mapped_data.loc[station_rows, "Station"] = station_name
     else:
         raise ValueError(
             "Either set parameters 'canton' and 'nr_ranges', or 'cantons'!"
         )
+    return mapped_data
 
 
 info("Map SPR+ data to Intervista Stations...")
-map_stations(
-    cantons={
-        "AG": "Aarau",
-        "BS": "Basel SBB",
-        "FR": "Fribourg",
-        "GR": "Chur",
-        "LU": "Luzern",
-        "NE": "Neuchatel",
-        "SG": "St. Gallen",
-        "SO": "Olten",
-        "VS": "Brig",
-        "ZG": "Zug",
-    }
-)
-map_stations(
-    canton="ZH",
-    nr_ranges={
-        NrRange(680759, 680831): "Zürich HB",
-        NrRange(680846, 680848): "Zürich Stadelhofen",
-        NrRange(680858, 680858): "Zürich Enge",
-        NrRange(680873, 680876): "Winterthur",
-        NrRange(688299, 688303): "Zürich Flughafen",
-        NrRange(689938, 690717): "Zürich HB",  # Sihlquai-Bahnhof, Gleis-Unterführungen
-        NrRange(690723, 690723): "Winterthur",
-        NrRange(690910, 690922): "Zürich HB",  # Bahnhofplatz
-        NrRange(705698, 705701): "Winterthur",
-        NrRange(705702, 705703): "Zürich Stadelhofen",
-        NrRange(705704, 705705): "Zürich Oerlikon",
-    },
-)
-map_stations(canton="GE", nr_ranges={NrRange(680802, 690726): "Genève Cornavin",})
-map_stations(
-    canton="VD",
-    nr_ranges={
-        NrRange(680797, 711299): "Lausanne",
-        NrRange(711300, 711308): "M2",  # M2 Gare Lausanne
-        NrRange(711309, 711314): "M2",  # M2 Grancy
-    },
-)
-map_stations(
-    canton="BE",
-    nr_ranges={
-        NrRange(680778, 680862): "Bern",
-        NrRange(690699, 690699): "Biel/Bienne",
-        NrRange(691900, 691903): "Bern",
-        NrRange(711340, 711341): "Biel/Bienne",
-        NrRange(721646, 721700): "Bern",
-    },
-)
-map_stations(
-    canton="TI",
-    nr_ranges={
-        NrRange(711180, 711180): "Bellinzona",
-        NrRange(713103, 713106): "Lugano",
-    },
+spr_data = (
+    spr_data.pipe(
+        map_stations,
+        cantons={
+            "AG": "Aarau",
+            "BS": "Basel SBB",
+            "FR": "Fribourg",
+            "GR": "Chur",
+            "LU": "Luzern",
+            "NE": "Neuchatel",
+            "SG": "St. Gallen",
+            "SO": "Olten",
+            "VS": "Brig",
+            "ZG": "Zug",
+        },
+    )
+    .pipe(
+        map_stations,
+        canton="ZH",
+        nr_ranges={
+            NrRange(680759, 680831): "Zürich HB",
+            NrRange(680846, 680848): "Zürich Stadelhofen",
+            NrRange(680858, 680858): "Zürich Enge",
+            NrRange(680873, 680876): "Winterthur",
+            NrRange(688299, 688303): "Zürich Flughafen",
+            NrRange(689938, 690717): "Zürich HB",  # Sihlquai, Gleis-Unterführungen
+            NrRange(690723, 690723): "Winterthur",
+            NrRange(690910, 690922): "Zürich HB",  # Bahnhofplatz
+            NrRange(705698, 705701): "Winterthur",
+            NrRange(705702, 705703): "Zürich Stadelhofen",
+            NrRange(705704, 705705): "Zürich Oerlikon",
+        },
+    )
+    .pipe(
+        map_stations,
+        canton="GE",
+        nr_ranges={NrRange(680802, 690726): "Genève Cornavin",},
+    )
+    .pipe(
+        map_stations,
+        canton="VD",
+        nr_ranges={
+            NrRange(680797, 711299): "Lausanne",
+            NrRange(711300, 711308): "M2",  # M2 Gare Lausanne
+            NrRange(711309, 711314): "M2",  # M2 Grancy
+        },
+    )
+    .pipe(
+        map_stations,
+        canton="BE",
+        nr_ranges={
+            NrRange(680778, 680862): "Bern",
+            NrRange(690699, 690699): "Biel/Bienne",
+            NrRange(691900, 691903): "Bern",
+            NrRange(711340, 711341): "Biel/Bienne",
+            NrRange(721646, 721700): "Bern",
+        },
+    )
+    .pipe(
+        map_stations,
+        canton="TI",
+        nr_ranges={
+            NrRange(711180, 711180): "Bellinzona",
+            NrRange(713103, 713106): "Lugano",
+        },
+    )
 )
 
 # Check if we matched all of them
