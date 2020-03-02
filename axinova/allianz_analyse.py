b1275cff51cbb9595b5dd1670db0b89fc31b3e2d
kpflugshaupt <kpflugshaupt@gmail.com>
2020-03-02 09:51:02 +0100

    Reformattings, renames, better comments

--- axinova/allianz_analyse.py
+++ axinova/allianz_analyse.py
@@ -4,38 +4,38 @@ from UpliftTarget import And, Variable
 from UpliftWebApp import calculate_target, describe_target, show_plots, show_results
 
 
+# define target groups
 @st.cache(allow_output_mutation=True)
 def create_targets() -> dict:
-    all_targets = dict()
-    all_targets["age_25_44"] = Variable(
-        "Age 25-44", variable="md_agenatrep", code_nr=[1]
+    targets = dict(
+        age_25_44=Variable(name="Age 25-44", variable="md_agenatrep", code_nr=[1]),
+        age_45_54=Variable(name="Age 45-54", variable="md_agenatrep", code_nr=[2]),
+        wohnen=And(
+            name="Wohnen",
+            target1=Variable(name="Kind < 18", variable="md_hhu18", code_nr=[0]),
+            target2=Variable(name="Haushalt > 2", variable="md_hhgr3", code_nr=[2]),
+        ),
+        KMU=Variable(name="KMU", variable="md_920", code_nr=[1]),
     )
-    all_targets["age_45_54"] = Variable(
-        "Age 45-54", variable="md_agenatrep", code_nr=[2]
-    )
-    all_targets["wohnen"] = And(
-        "Wohnen",
-        Variable("Kind < 18", variable="md_hhu18", code_nr=[0]),
-        Variable("Haushalt > 2", variable="md_hhgr3", code_nr=[2]),
-    )
-    all_targets["KMU"] = Variable("KMU", variable="md_920", code_nr=[1])
-    return all_targets
+    return targets
 
-
-TARGETS = create_targets()
 
 # choose target group
+all_targets = create_targets()
 target_key = st.selectbox(
     label="Choose Target Group:",
-    options=list(TARGETS.keys()),
+    options=list(all_targets.keys()),
     index=0,
-    format_func=lambda key: TARGETS[key].name,
+    format_func=lambda key: all_targets[key].name,
 )
 
-# calculate ratios
-target = calculate_target(TARGETS[target_key])
+# calculate ratios for selected target
+target = calculate_target(all_targets[target_key])
 
-# show it
+# show results
 describe_target(target)
 show_results(target)
 show_plots(target)
+
+if st.button("Store results as XLSX"):
+    target.export_result()
