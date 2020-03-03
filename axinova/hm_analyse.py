fd3cc91014c9b043e71fd7e7199dc116900362ca
kpflugshaupt <kpflugshaupt@gmail.com>
2020-03-03 18:36:49 +0100

    Upgrade to current Way Of Things

--- axinova/hm_analyse.py
+++ axinova/hm_analyse.py
@@ -1,92 +1,66 @@
 import streamlit as st
 
 from UpliftTarget import Variable, And, Or, source_data
+from UpliftWebApp import calculate_target, describe_target, show_plots, show_results
 
 
+# define target groups
+@st.cache(allow_output_mutation=True)
 def create_targets() -> dict:
-    all_targets = dict()
-    all_targets["jung_hoch"] = Variable(
-        "Jung, EK hoch", variable="md_SexAgeEk", code_nr=[4, 5, 32, 33]
-    )
-    all_targets["jung_w_mittelhoch"] = Variable(
-        "Jung, w, EK mittel/hoch", variable="md_SexAgeEk", code_nr=[30, 31, 32, 33]
-    )
-    all_targets["jung_niedrig"] = Variable(
-        "Jung, EK niedrig", variable="md_SexAgeEk", code_nr=[0, 1, 28, 29]
-    )
-    all_targets["jung_m_niedrig"] = Variable(
-        "Jung, M, EK niedrig", variable="md_SexAgeEk", code_nr=[0, 1]
-    )
-    all_targets["jung_w_niedrig"] = Variable(
-        "Jung, W, EK niedrig", variable="md_SexAgeEk", code_nr=[28, 29]
-    )
-    all_targets["jung_beauty"] = And(
-        "Jung, Beauty/Fashion",
-        Variable("Jung", variable="md_agenatrep", code_nr=[0]),
-        Or(
-            "Beauty, Fashion",
-            Variable("Shopping", "md_875", [1]),
-            Variable("Kosmetik", "md_855", [1]),
+    targets = dict(
+        frauen_18_44=Variable(
+            "Frauen 18-44 Jahre", variable="md_SexAgeEk", code_nr=list(range(28, 42))
+        ),
+        personen_39_55_1=Variable(
+            "Personen 39-55 Jahre (eng)", variable="md_agenatrep", code_nr=[2]
+        ),
+        personen_39_55_2=Variable(
+            "Personen 39-55 Jahre (weit)", variable="md_agenatrep", code_nr=[1, 2]
+        ),
+        jung_hoch=Variable(
+            "Jung, EK hoch", variable="md_SexAgeEk", code_nr=[4, 5, 32, 33]
+        ),
+        jung_w_mittelhoch=Variable(
+            "Jung, w, EK mittel/hoch", variable="md_SexAgeEk", code_nr=[30, 31, 32, 33]
+        ),
+        jung_niedrig=Variable(
+            "Jung, EK niedrig", variable="md_SexAgeEk", code_nr=[0, 1, 28, 29]
+        ),
+        jung_m_niedrig=Variable(
+            "Jung, M, EK niedrig", variable="md_SexAgeEk", code_nr=[0, 1]
+        ),
+        jung_w_niedrig=Variable(
+            "Jung, W, EK niedrig", variable="md_SexAgeEk", code_nr=[28, 29]
+        ),
+        jung_beauty=And(
+            "Jung, Beauty/Fashion",
+            Variable("Jung", variable="md_agenatrep", code_nr=[0]),
+            Or(
+                "Beauty, Fashion",
+                Variable("Shopping", "md_875", [1]),
+                Variable("Kosmetik", "md_855", [1]),
+            ),
         ),
     )
-    return all_targets
+    return targets
 
 
-def calculate_target(key: str) -> Variable:
-    global TARGETS
-    result = targets[key]
-    result.set_timescale("Hour")
-    result.calculate()
-    return result
-
-
-TARGETS = create_targets()
-
-# Title
-st.markdown("# Zielgruppen-Analyse")
-st.sidebar.markdown("### Parameters")
-
-# Choose Target Group
+# choose target group
+all_targets = create_targets()
 target_key = st.selectbox(
     label="Choose Target Group:",
-    options=list(TARGETS.keys()),
+    options=list(all_targets.keys()),
     index=0,
-    format_func=lambda key: TARGETS[key].name,
-)
-target = calculate_target(target_key)
-st.markdown(f"## Target Group: {target.name}")
-
-st.markdown("### Definition:")
-st.text(target.description())
-
-# Result Tables
-st.markdown("### Summary:")
-st.table(target.result_summary())
-
-st.markdown("### Stations per week:")
-st.table(target.best_stations(where="spr > 0"))
-
-nr_best_slots = st.sidebar.number_input(
-    "Number of Best Slots:", min_value=1, max_value=500, value=20, format="%d"
-)
-target_min = st.sidebar.number_input(
-    "Minimum Target Persons:", min_value=0, max_value=1000, value=360, format="%d"
-)
-st.markdown(f"### Best Slots by Ratio (Target Persons >= {target_min}):")
-st.table(
-    target.best_slots(
-        column=["pop_uplift_ratio", "target_pers"],
-        where=f"target_pers >= {target_min}",
-        top_n=nr_best_slots,
-    )
+    format_func=lambda key: all_targets[key].name,
 )
 
-# Result Plot
-station_list = st.sidebar.multiselect(
-    label="Interesting Stations:",
-    options=source_data.all_stations,
-    default=["Basel SBB", "Bern", "Genève Cornavin", "Winterthur", "Zürich HB"],
-)
-st.markdown(f"### Plots für {', '.join(station_list)}:")
-barplot = target.plot_ch_uplift_barplot(selectors={"Station": station_list})
-st.altair_chart(barplot, use_container_width=False)
+# calculate ratios for selected target
+target = calculate_target(all_targets[target_key])
+
+# show results
+describe_target(target)
+show_results(target)
+show_plots(target)
+
+if st.button("Store results as XLSX"):
+    target.export_result()
