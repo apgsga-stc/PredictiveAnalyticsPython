8aa8b27227aec6668ee0d5d8097c0fe3c8a8bd53
apgsga-stc <stc@apgsga.ch>
2020-03-16 09:53:27 +0100

    Refactor: outsourced ek_list preparation into its own file.

--- vkprog_analyse/vkprog_master_script.py
+++ vkprog_analyse/vkprog_master_script.py
@@ -25,7 +25,6 @@ from sklearn.metrics import classification_report, average_precision_score
 # Utilities:
 from pa_lib.file import project_dir, store_bin
 from pa_lib.job import request_job
-from pa_lib.file import load_bin
 from pa_lib.log import info
 
 # Special libs:
@@ -39,6 +38,8 @@ from vkprog_analyse.vkprog_model_validation import (
     confusion_matrices,
     roc_curve_graph,
 )
+
+from vkprog_analyse.vkprog_dataprep_ek_list import add_ek_info
 
 info("vkprog_master_script.py: START")
 
@@ -238,8 +239,6 @@ info("Model Validation")
 
 ## Confusion Matrix:
 
-#confusion_matrices(x_test=X_train_balanced, y_test=y_train_balanced, model=forest_01)
-#confusion_matrices(x_test=X_train, y_test=y_train, model=forest_01)
 confusion_matrices(x_test=X_test, y_test=y_test, model=forest_01)
 
 ## Classification Report:
@@ -255,8 +254,6 @@ print(
 
 ## Precision-Recall Curve:
 
-#prec_rec_curve(x_train=X_train_balanced, y_train=y_train_balanced, model=forest_01)
-#prec_rec_curve(x_train=X_train, y_train=y_train, model=forest_01)
 prec_rec_curve(x_train=X_test, y_train=y_test, model=forest_01)
 
 ## Average Precision:
@@ -268,87 +265,27 @@ info(f"Average Precision of forest_01: {avg_precision_forest_01}"[:37])
 
 ## Receiver Operating Characteristics (ROC) and AUC:
 
-#roc_curve_graph(X_train_balanced, y_train_balanced, model=forest_01)
-#roc_auc(X_train_balanced, y_train_balanced, forest_01)
-
-#roc_curve_graph(X_train, y_train, model=forest_01)
-#roc_auc(X_train, y_train, forest_01)
-
 roc_curve_graph(X_test, y_test, model=forest_01)
 roc_auc(X_test, y_test, forest_01)
 
 ########################################################################################
 # Scoring:
 ########################################################################################
-
 # Score Class Probabilities (Booking: No/Yes)
 
 scoring_prob = forest_01.predict_proba(X_scoring)
-
 scoring_prob_df = pd.DataFrame(scoring_prob, columns=["Prob_0", "Prob_1"])
-
 scoring_all_prob = pd.merge(
     scoring_all, scoring_prob_df, left_index=True, right_index=True
 ).sort_values("Prob_1", ascending=False)
+
+info(f"scoring_all_prob.shape: {scoring_all_prob.shape}")
 
 ########################################################################################
 # Preparation: Deployment
 ########################################################################################
 
-ek_info = load_bin("vkprog/ek_info.feather")
-ek_list_raw = pd.merge(scoring_all_prob, ek_info, on="Endkunde_NR", how="left")
-
-net_columns = [col for col in ek_info.columns if col.startswith("Net_")]
-
-col_row_filter = [
-    "Insolvenz",
-    # "Last_Res_Date",  # covered in listing
-    # "Last_Aus_Date",  # covered in listing
-    "last_CRM_Ktkt_date",
-    "VB_FILTER_VON",
-    "VB_FILTER_BIS",
-]
-
-relevant_cols_deploy = (
-    [
-        "Endkunde_NR",  # Endkunde_NR
-        "Endkunde",  # Endkunde
-        "EK_HB_Apg_Kurzz",  # HB_APG (based on R-script)
-        "Agentur",  # Agentur
-        "AG_Hauptbetreuer",  # HB_Agentur
-        "PLZ",  # PLZ
-        "GEMEINDE",
-    ]  # Ort
-    + net_columns  # Net_2015, Net_2016, Net_2017, Net_2018, Net_2019
-    + [
-        "letzte_VBs",  # (bd, aggregiert)
-        "Datum_Letzter_Ktkt",  # letzter Ktkt (Datum, crm)
-        "Letzter_Kontakt",  # KZ_letzter_Ktkt (crm)
-        "Kanal",  # (crm)
-        "Betreff",  # (crm)
-        "Last_Res_Date",  # Letzte_Kamp_erfasst
-        "Last_Aus_Date",  # letzte_Kamp_Beginn
-        "VERKAUFS_GEBIETS_CODE",  # Verkaufsgebiet
-        "VB_VKGEB",
-        "Prob_1",  # prob_KW (from here, good good.)
-    ]
-    # Needed for row_filter
-    + col_row_filter
-)
-
-ek_list = ek_list_raw.loc[:, relevant_cols_deploy].rename(
-    columns={
-        "EK_HB_Apg_Kurzz": "HB_APG",
-        "AG_Hauptbetreuer": "HB_Agentur",
-        "GEMEINDE": "Ort",
-        "Letzter_Kontakt": "letzter_Kontakt",
-        "Last_Res_Date": "letzte_Kamp_erfasst",
-        "Last_Aus_Date": "letzte_Kamp_Beginn",
-        "VERKAUFS_GEBIETS_CODE": "Verkaufsgebiet",
-        "VB_VKGEB": "VB_VK_Geb",
-        "Prob_1": "prob_KW",
-    }
-)
+ek_list = add_ek_info(scored_dataframe=scoring_all_prob)
 
 info(f"ek_list.shape: {ek_list.shape}")
 
