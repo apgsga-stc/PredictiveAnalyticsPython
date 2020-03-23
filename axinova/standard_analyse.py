87d0c7c4d1d5be8b896eba5d80b56be83b4af195
kpflugshaupt <kpflugshaupt@gmail.com>
2020-03-23 18:20:58 +0100

    reformat graphs, make zip file writing more safe

--- axinova/standard_analyse.py
+++ axinova/standard_analyse.py
@@ -1,110 +1,98 @@
 import streamlit as st
 
-from UpliftTarget import Variable, And, Or, source_data
+from UpliftTarget import And, Or, Variable
+from UpliftWebApp import (
+    calculate_target,
+    choose_stations,
+    describe_target,
+    download_results,
+    show_station_heatmaps,
+    download_station_heatmaps,
+    show_station_weekdays,
+    show_summary,
+    show_timeslot_plots,
+    download_timeslot_plots,
+    show_timeslots,
+)
 
 
+# define target groups
+@st.cache(allow_output_mutation=True)
 def create_targets() -> dict:
-    all_targets = dict()
-    all_targets["automotive"] = And(
-        "Automotive",
-        Or(
-            "Auto nicht billig",
-            Variable(name="Teures Auto", variable="g_CarHigh", code_nr=[1]),
-            Variable(name="Mittelteures Auto", variable="g_CarMedium", code_nr=[1]),
+    targets = dict(
+        automotive=And(
+            "Automotive",
+            Or(
+                "Auto nicht billig",
+                Variable(name="Teures Auto", variable="g_CarHigh", code_nr=[1]),
+                Variable(name="Mittelteures Auto", variable="g_CarMedium", code_nr=[1]),
+            ),
+            Variable(name="Autobesitz", variable="g_220", code_nr=[1, 2]),
         ),
-        Variable(name="Autobesitz", variable="g_220", code_nr=[1, 2]),
-    )
-    all_targets["sportinteressiert"] = And(
-        "Sportinteresse",
-        Variable("Sportveranstaltungen", "md_872", [1]),
-        Or(
-            "Aktivsport",
-            Variable("Einzelsport", "g_sportSingle", [1]),
-            Variable("Teamsport", "g_sportMulti", [1]),
+        sportinteressiert=And(
+            "Sportinteresse",
+            Variable("Sportveranstaltungen", "md_872", [1]),
+            Or(
+                "Aktivsport",
+                Variable("Einzelsport", "g_sportSingle", [1]),
+                Variable("Teamsport", "g_sportMulti", [1]),
+            ),
         ),
-    )
-    all_targets["finance"] = And(
-        "Finance",
-        Variable("Hohes Einkommen", "md_ek", [4, 5]),
-        Variable("Grosses Vermögen", "md_hhverm", [4, 5, 6]),
-    )
-    all_targets["lifestyle"] = Or(
-        "Lifestyle",
-        Variable("Pop-Konzerte", "md_877", [1]),
-        Or(
-            "Lifestyle3",
-            Variable("Discos, Clubs", "md_873", [1]),
-            Variable("Bars, Kneipen", "md_874", [1]),
+        finance=And(
+            "Finance",
+            Variable("Hohes Einkommen", "md_ek", [4, 5]),
+            Variable("Grosses Vermögen", "md_hhverm", [4, 5, 6]),
         ),
+        lifestyle=Or(
+            "Lifestyle",
+            Variable("Pop-Konzerte", "md_877", [1]),
+            Or(
+                "Lifestyle3",
+                Variable("Discos, Clubs", "md_873", [1]),
+                Variable("Bars, Kneipen", "md_874", [1]),
+            ),
+        ),
+        beauty_fashion=Or(
+            "Beauty, Fashion",
+            Variable("Shopping", "md_875", [1]),
+            Variable("Kosmetik", "md_855", [1]),
+        ),
+        reisen=Or(
+            "Reisen",
+            Variable("Vielflieger", "g_flug", [2, 3]),
+            Variable("Zugfahrer", "g_privatetrainuse", [3, 4]),
+        ),
+        vermoegend=Variable("Vermögen > 3 Mio", "md_hhverm", [5, 6]),
     )
-    all_targets["beauty_fashion"] = Or(
-        "Beauty, Fashion",
-        Variable("Shopping", "md_875", [1]),
-        Variable("Kosmetik", "md_855", [1]),
-    )
-    all_targets["reisen"] = Or(
-        "Reisen",
-        Variable("Vielflieger", "g_flug", [2, 3]),
-        Variable("Zugfahrer", "g_privatetrainuse", [3, 4]),
-    )
-    return all_targets
+    return targets
 
 
-def calculate_target(key: str) -> Variable:
-    global TARGETS
-    result = targets[key]
-    result.set_timescale("Hour")
-    result.calculate()
-    return result
-
-
-TARGETS = create_targets()
-
-# Title
-st.markdown("# Zielgruppen-Analyse")
-st.sidebar.markdown("### Parameters")
-
-# Choose Target Group
+# choose target group
+all_targets = create_targets()
 target_key = st.selectbox(
     label="Choose Target Group:",
-    options=list(TARGETS.keys()),
+    options=list(all_targets.keys()),
     index=0,
-    format_func=lambda key: TARGETS[key].name,
-)
-target = calculate_target(target_key)
-st.markdown(f"## Target Group: {target.name}")
-
-st.markdown("### Definition:")
-st.text(target.description())
-
-# Result Tables
-st.markdown("### Summary:")
-st.table(target.result_summary())
-
-st.markdown("### Stations per week:")
-st.table(target.best_stations(where="spr > 0"))
-
-nr_best_slots = st.sidebar.number_input(
-    "Number of Best Slots:", min_value=1, max_value=500, value=20, format="%d"
-)
-target_min = st.sidebar.number_input(
-    "Minimum Target Persons:", min_value=0, max_value=1000, value=360, format="%d"
-)
-st.markdown(f"### Best Slots by Ratio (Target Persons >= {target_min}):")
-st.table(
-    target.best_slots(
-        column=["pop_uplift_ratio", "target_pers"],
-        where=f"target_pers >= {target_min}",
-        top_n=nr_best_slots,
-    )
+    format_func=lambda key: all_targets[key].name,
 )
 
-# Result Plot
-station_list = st.sidebar.multiselect(
-    label="Interesting Stations:",
-    options=source_data.all_stations,
-    default=["Basel SBB", "Bern", "Genève Cornavin", "Winterthur", "Zürich HB"],
-)
-st.markdown(f"### Plots für {', '.join(station_list)}:")
-barplot = target.plot_timeslot_plots(selectors={"Station": station_list})
-st.altair_chart(barplot, use_container_width=False)
+# calculate ratios for selected target
+target = calculate_target(all_targets[target_key])
+download_results(target)
+
+# show result header
+station_list = choose_stations()
+describe_target(target)
+
+# choose detail result to show
+results = {
+    "Summary": lambda tgt, _: show_summary(tgt),
+    "Stations / Weekdays": lambda tgt, sl: show_station_weekdays(tgt, sl),
+    "Station Heatmaps: Zielkontakte": lambda tgt, sl: show_station_heatmaps(tgt, sl),
+    "Download Station Heatmaps": lambda tgt, sl: download_station_heatmaps(tgt, sl),
+    "Best Timeslots": lambda tgt, _: show_timeslots(tgt),
+    "Timeslot Plots": lambda tgt, sl: show_timeslot_plots(tgt, sl),
+    "Download Timeslot Plots": lambda tgt, sl: download_timeslot_plots(tgt, sl),
+}
+result_key = st.selectbox("Choose result:", options=list(results.keys()))
+results[result_key](target, station_list)
